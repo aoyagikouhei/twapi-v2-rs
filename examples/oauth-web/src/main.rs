6c70037a9aa43bb5bcafc697f96954b3331ebcf8
aoyagikouhei <aoyagi.kouhei@gmail.com>
2023-04-24 13:37:19 +0900

    add example oauth web

---
+++ examples/oauth-web/src/main.rs
@@ -0,0 +1,22 @@
+use axum::{
+    routing::{get, },
+    Router,
+};
+
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
