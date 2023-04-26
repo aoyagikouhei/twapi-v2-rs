c99ae32b0bece3575758ea651544792936019c01
aoyagikouhei <aoyagi.kouhei@gmail.com>
2023-04-26 12:02:47 +0900

    add 2 tests

---
+++ tests/get_2_users_by_username_username.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+use twapi_v2::api::{execute_twitter, get_2_users_by_username_username};
+
+// BEARER_CODE=XXXXX cargo test test_get_2_users_by_username_username -- --nocapture --test-threads=1
+
+#[tokio::test]
+async fn test_get_2_users_by_username_username() -> Result<()> {
+    let bearer_code = std::env::var("BEARER_CODE").unwrap_or_default();
+    let builder =
+        get_2_users_by_username_username::Api::open(&bearer_code, "yukihiro_matz").build();
+    let (res, _rate_limit) = execute_twitter::<serde_json::Value>(builder).await?;
+    println!("{}", serde_json::to_string(&res).unwrap());
+    let response = serde_json::from_value::<get_2_users_by_username_username::Response>(res)?;
+    let data = response.data.as_ref().unwrap();
+    assert_eq!(data.username, "yukihiro_matz");
+    assert_eq!(response.is_empty_extra(), true);
+    Ok(())
+}
