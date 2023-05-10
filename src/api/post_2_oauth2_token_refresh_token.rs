4f1fe3c053db587d1545a61d0e12c95afaa3eece
aoyagikouhei <aoyagi.kouhei@gmail.com>
2023-05-10 16:23:31 +0900

    add refresh token

--- src/api/post_2_oauth2_token_refresh_token.rs
+++ src/api/post_2_oauth2_token_refresh_token.rs
@@ -4,44 +4,18 @@ use serde::{Deserialize, Serialize};
 
 const URL: &str = "https://api.twitter.com/2/oauth2/token";
 
-#[derive(Serialize, Deserialize, Debug, Eq, Hash, PartialEq, Clone)]
-pub enum GrantType {
-    RefreshToken,
-}
-
-impl std::fmt::Display for GrantType {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Self::RefreshToken => write!(f, "refresh_token"),
-        }
-    }
-}
-
-impl Default for GrantType {
-    fn default() -> Self {
-        Self::RefreshToken
-    }
-}
-
 #[derive(Debug, Clone, Default)]
 pub struct Api {
     api_key_code: String,
     api_secret_code: String,
-    grant_type: GrantType,
     refresh_token: String,
 }
 
 impl Api {
-    pub fn new(
-        api_key_code: &str,
-        api_secret_code: &str,
-        grant_type: GrantType,
-        refresh_token: &str,
-    ) -> Self {
+    pub fn new(api_key_code: &str, api_secret_code: &str, refresh_token: &str) -> Self {
         Self {
             api_key_code: api_key_code.to_owned(),
             api_secret_code: api_secret_code.to_owned(),
-            grant_type,
             refresh_token: refresh_token.to_owned(),
         }
     }
@@ -49,7 +23,7 @@ impl Api {
     pub fn build(self) -> RequestBuilder {
         let form_paramters = vec![
             ("client_id", self.api_key_code.clone()),
-            ("grant_type", self.grant_type.to_string()),
+            ("grant_type", "refresh_token".to_owned()),
             ("refresh_token", self.refresh_token),
         ];
 
--- tests/post_2_oauth2_token_refresh_token.rs
+++ tests/post_2_oauth2_token_refresh_token.rs
@@ -1,10 +1,10 @@
 use anyhow::Result;
-use twapi_v2::api::{execute_twitter, post_2_oauth2_token};
+use twapi_v2::api::{execute_twitter, post_2_oauth2_token_refresh_token};
 
-// API_KEY_CODE=XXXXX API_SECRET_CODE=XXXXX REFRESH_TOKEN=XXXXX cargo test test_post_2_oauth2_token -- --nocapture --test-threads=1
+// API_KEY_CODE=XXXXX API_SECRET_CODE=XXXXX REFRESH_TOKEN=XXXXX cargo test test_post_2_oauth2_token_refresh_token -- --nocapture --test-threads=1
 
 #[tokio::test]
-async fn test_post_2_oauth2_token() -> Result<()> {
+async fn test_post_2_oauth2_token_refresh_token() -> Result<()> {
     let api_key_code = match std::env::var("API_KEY_CODE") {
         Ok(api_key_code) => api_key_code,
         _ => return Ok(()),
@@ -17,16 +17,15 @@ async fn test_post_2_oauth2_token() -> Result<()> {
         Ok(refresh_token) => refresh_token,
         _ => return Ok(()),
     };
-    let builder = post_2_oauth2_token::Api::new(
+    let builder = post_2_oauth2_token_refresh_token::Api::new(
         &api_key_code,
         &api_secret_code,
-        post_2_oauth2_token::GrantType::RefreshToken,
         &refresh_token,
     )
     .build();
     let (res, _rate_limit) = execute_twitter::<serde_json::Value>(builder).await?;
     println!("{}", serde_json::to_string(&res).unwrap());
-    let response = serde_json::from_value::<post_2_oauth2_token::Response>(res)?;
+    let response = serde_json::from_value::<post_2_oauth2_token_refresh_token::Response>(res)?;
     assert_eq!(response.is_empty_extra(), true);
     Ok(())
 }
