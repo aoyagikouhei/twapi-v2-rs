73c118f13ea9570eef081ccdfdedc45496c4f42b
Kouhei Aoyagi <aoyagi_kouhei@uniquevision.co.jp>
2024-02-05 09:25:47 +0900

    add readme

---
+++ maker/README.md
@@ -0,0 +1,9 @@
+# rust
+```
+bundle exec ruby maker.rs
+```
+
+# typescript
+```
+bundle exec ruby ../typescript/maker.rs
+```
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -5,3 +5,4 @@ import {objectToCamel} from 'ts-case-convert'
 const data = fs.readFileSync('./data.json', 'utf-8')
 const data2: Tweets = objectToCamel(JSON.parse(data)) as any
 console.log(data2)
+console.log(data2.contextAnnotations[0].domain)
