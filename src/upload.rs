0ecb004968cb5e9aff9b84217d0261b3d7e92b7d
Kouhei Aoyagi <aoyagi_kouhei@uniquevision.co.jp>
2024-03-26 08:09:26 +0900

    add init

--- src/upload.rs
+++ src/upload.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 pub mod post_media_uploda_init;
 
 const ENV_KEY: &str = "TWAPI_V2_MEDIA_API_PREFIX_API";
@@ -14,4 +16,74 @@ pub fn setup_prefix_url(url: &str) {
 pub(crate) fn make_url<S: AsRef<str>>(post_url: S) -> String {
     let prefix_url = std::env::var(ENV_KEY).unwrap_or(PREFIX_URL_MEDIA.to_owned());
     format!("{}{}", prefix_url, post_url.as_ref())
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum MediaCategory {
+    AmplifyVideo,
+    TweetGif,
+    TweetImage,
+    TweetVideo,
+}
+
+impl std::fmt::Display for MediaCategory {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let value = match self {
+            Self::AmplifyVideo => "amplify_video",
+            Self::TweetGif => "tweet_gif",
+            Self::TweetImage => "tweet_image",
+            Self::TweetVideo => "tweet_video",
+        };
+        write!(f, "{}", value)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct Image {
+    pub image_type: String,
+    pub w: u64,
+    pub h: u64,
+    #[serde(flatten)]
+    pub extra: std::collections::HashMap<String, serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct Video {
+    pub video_type: String,
+    #[serde(flatten)]
+    pub extra: std::collections::HashMap<String, serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct ProcessingInfo {
+    pub state: String,
+    pub check_after_secs: Option<u64>,
+    pub progress_percent: Option<u64>,
+    #[serde(flatten)]
+    pub extra: std::collections::HashMap<String, serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct Response {
+    pub media_id: u64,
+    pub media_id_string: String,
+    pub expires_after_secs: u64,
+    pub media_key: Option<String>,
+    pub size: Option<u64>,
+    pub image: Option<Image>,
+    pub video: Option<Video>,
+    pub processing_info: Option<ProcessingInfo>,
+    #[serde(flatten)]
+    pub extra: std::collections::HashMap<String, serde_json::Value>,
+}
+
+impl Response {
+    pub fn is_empty_extra(&self) -> bool {
+        let res = self.extra.is_empty();
+        if !res {
+            println!("Response {:?}", self.extra);
+        }
+        res
+    }
 }
--- src/upload/post_media_uploda_init.rs
+++ src/upload/post_media_uploda_init.rs
@@ -2,33 +2,31 @@ use crate::{
     api::{execute_twitter, Authentication},
     error::Error,
     headers::Headers,
-    upload::make_url,
+    upload::{make_url, MediaCategory, Response},
 };
-use reqwest::RequestBuilder;
-use serde::{Deserialize, Serialize};
+use reqwest::{multipart::Form, RequestBuilder};
 
 const URL: &str = "/1.1/media/upload.json";
 
 #[derive(Debug, Clone, Default)]
-pub struct Form {
-    total_bytes: u64,
-    media_type: String,
-    media_category: Option<String>,
-    additional_owners: Option<String>,
+pub struct Data {
+    pub total_bytes: u64,
+    pub media_type: String,
+    pub media_category: Option<MediaCategory>,
+    pub additional_owners: Option<String>,
 }
 
-impl Form {
-    fn make_form(self) -> Vec<(&'static str, String)> {
-        let mut res = vec![
-            ("command", "INIT".to_owned()),
-            ("total_bytes", self.total_bytes.to_string()),
-            ("media_type", self.media_type),
-        ];
+impl Data {
+    fn make_form(self) -> Form {
+        let mut res = Form::new()
+            .text("command", "INIT")
+            .text("total_bytes", self.total_bytes.to_string())
+            .text("media_type", self.media_type);
         if let Some(media_category) = self.media_category {
-            res.push(("media_category", media_category));
+            res = res.text("media_category", media_category.to_string());
         }
         if let Some(additional_owners) = self.additional_owners {
-            res.push(("additional_owners", additional_owners));
+            res = res.text("additional_owners", additional_owners);
         }
         res
     }
@@ -36,18 +34,18 @@ impl Form {
 
 #[derive(Debug, Clone, Default)]
 pub struct Api {
-    form: Form,
+    data: Data,
 }
 
 impl Api {
-    pub fn new(form: Form) -> Self {
-        Self { form }
+    pub fn new(data: Data) -> Self {
+        Self { data }
     }
 
     pub fn build(self, authentication: &impl Authentication) -> RequestBuilder {
         let client = reqwest::Client::new();
         let url = make_url(URL);
-        let builder = client.post(&url).form(&self.form.make_form());
+        let builder = client.post(&url).multipart(self.data.make_form());
         authentication.execute(builder, "POST", &url, &[])
     }
 
@@ -56,32 +54,5 @@ impl Api {
         authentication: &impl Authentication,
     ) -> Result<(Response, Headers), Error> {
         execute_twitter(self.build(authentication)).await
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Default)]
-pub struct Image {
-    pub image_type: String,
-    pub w: u64,
-    pub h: u64,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Default)]
-pub struct Response {
-    pub media_id: u64,
-    pub media_id_string: String,
-    pub size: u64,
-    pub image: Option<Image>,
-    #[serde(flatten)]
-    pub extra: std::collections::HashMap<String, serde_json::Value>,
-}
-
-impl Response {
-    pub fn is_empty_extra(&self) -> bool {
-        let res = self.extra.is_empty();
-        if !res {
-            println!("Response {:?}", self.extra);
-        }
-        res
     }
 }
