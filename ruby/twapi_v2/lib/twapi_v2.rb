f4add095f4a3212180d10f260ee43225bf2fa32b
Kouhei Aoyagi <aoyagi_kouhei@uniquevision.co.jp>
2024-04-26 15:57:29 +0900

    add ruby

---
+++ ruby/twapi_v2/lib/twapi_v2.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "twapi_v2/version"
+
+module TwapiV2
+  class Error < StandardError; end
+  # Your code goes here...
+
+  def self.hello
+    "Hello World"
+  end
+end
